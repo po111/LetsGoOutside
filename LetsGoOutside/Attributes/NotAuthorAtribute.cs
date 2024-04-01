019c16f9253a49194a6fd88c76410abc12b4dc9a
po111 <p_uzunova@abv.bg>
2024-04-01 23:35:47 +0300

    NotAuthor and NotOrganizer  attributes added

---
+++ LetsGoOutside/Attributes/NotAuthorAtribute.cs
@@ -0,0 +1,28 @@
+﻿using LetsGoOutside.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace LetsGoOutside.Attributes
+{
+    public class NotAuthorAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAuthorService? authorService = context.HttpContext.RequestServices.GetService<IAuthorService>();
+
+            if (authorService == null )
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (authorService !=null 
+                && authorService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
---
+++ LetsGoOutside/Attributes/NotOrganizerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using LetsGoOutside.Core.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LetsGoOutside.Attributes
+{
+    public class NotOrganizerAttribute :ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IOrganizerService? organizerService = context.HttpContext.RequestServices.GetService<IOrganizerService>();
+
+            if (organizerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (organizerService != null
+                && organizerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
