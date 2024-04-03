87a1e5365c45d678fc8fade3dec4aa8dd6073137
po111 <p_uzunova@abv.bg>
2024-04-03 22:13:05 +0300

    MustBeAuthor and MustBeOrganizer attributes added

---
+++ LetsGoOutside/Attributes/MustBeAuthorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using LetsGoOutside.Controllers;
+using LetsGoOutside.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace LetsGoOutside.Attributes
+{
+    public class MustBeAuthorAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAuthorService? authorService = context.HttpContext.RequestServices.GetService<IAuthorService>();
+
+            if (authorService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (authorService != null
+                && authorService.ExistsByIdAsync(context.HttpContext.User.Id()).Result==false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AuthorController.Become), "Author", null);
+            }
+        }
+    }
+}
---
+++ LetsGoOutside/Attributes/MustBeOrganizerAttribute.cs
@@ -0,0 +1,29 @@
+﻿using LetsGoOutside.Controllers;
+using LetsGoOutside.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace LetsGoOutside.Attributes
+{
+    public class MustBeOrganizerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IOrganizerService? organizerService = context.HttpContext.RequestServices.GetService<IOrganizerService>();
+
+            if (organizerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (organizerService != null
+                && organizerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result== false)
+            {
+                context.Result = new RedirectToActionResult(nameof(OrganizerController.Become), "Organizer", null);
+            }
+        }
+    }
+}
