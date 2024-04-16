89c0ecbc46ea57f9d5453281f71adb57cbb20ff7
po111 <p_uzunova@abv.bg>
2024-04-17 00:55:17 +0300

    First Tests and TesDB created

---
+++ LetsGoOutside.Tests/DbMockSeedData.cs
@@ -0,0 +1,29 @@
+﻿using LetsGoOutside.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsGoOutside.Tests
+{
+    public static class DbMockSeedData
+    {
+        public static List<Author> authors = new List<Author>()
+        {
+            new Author()
+            {
+                Name = "Евтим Добринов",
+            DateCreated = DateTime.Now.AddDays(-60),
+            UserId = "dea12856-c198-4129-b3f3-b893d8395082"
+            },
+
+            new Author()
+            {
+                 Name = "Лилия Друмева",
+                DateCreated = DateTime.Now.AddDays(-20),
+                UserId = "c974417d-38b4-42a6-9133-708d1d7c8b0f"
+            }
+        };
+    }
+}
